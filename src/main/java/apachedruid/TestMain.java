908f68031961eb7c577d966e315fec24e588e3dc
hngydxlinjing <1259478950@qq.com>
2020-06-29 13:27:28 +0800

    创建canal和apacheduid两

---
+++ src/main/java/apachedruid/TestMain.java
@@ -0,0 +1,12 @@
+package apachedruid;
+
+/**
+ * @author linjing
+ * @date: Created in 2020/6/29
+ */
+
+/**
+ * apacheduid :非德鲁伊，为apache的大数据处理框架，可以将数据以json格式导入，然后自动生成数据库，支持用Sql查询，导出结果
+ */
+public class TestMain {
+}
---
+++ src/main/java/canal/TestMain.java
@@ -0,0 +1,13 @@
+package canal;
+
+/**
+ * @author linjing
+ * @date: Created in 2020/6/29
+ */
+
+/**
+ * canal 阿里巴巴 数据库级别同步技术，基于日志，支持mysql,oracle
+ */
+public class TestMain {
+
+}
